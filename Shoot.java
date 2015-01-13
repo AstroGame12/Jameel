054c71171b737e40c7f57dc328d615c834e8eabd
JameelJiwani <jameeljiwani@gmail.com>
2015-01-12 20:36:43 -0500

    Create Shoot.java

---
+++ Shoot.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+/**
+ * Created by jameel on 09/01/15.
+ */
+
+ public class Shoot extends Rectangle {
+    public static Color clr = Color.BLUE;
+
+    public Shoot(int i, int i1, int i2, int i3) {
+        super(i, i1, i2, i3);
+    }
+}
